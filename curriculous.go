cb1b7633d4ea74c245d0244de1065e397f312b4e
Bastien Duplessier <bastien.duplessier@gmail.com>
2019-07-25 00:21:28 +0200

    add profiling to understand why is it slow

--- curriculous.go
+++ curriculous.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"curriculous/gif"
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 const font = "/usr/share/fonts/comic-sans/design.graffiti.comicsansms.ttf"
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+	flag.Parse()
 
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	var list = []string{"pipi", "caca", "prout", "cul", "nichon"}
 
